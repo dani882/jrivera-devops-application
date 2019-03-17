5671c6223cc2360cf38e1378865d1b7fae73cb1d
Jesus Rivera <dani882@gmail.com>
2019-03-17 12:50:34 -0400

    Modified files

--- users/config/sequelize_db_settings.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  username: process.env.DEVOPS_TEST_DATABASE_USERNAME || 'postgres',
-  password: process.env.DEVOPS_TEST_DATABASE_PASSWORD || '',
-  database: process.env.DEVOPS_TEST_DATABASE_NAME || 'devops_test',
-  host: process.env.DEVOPS_TEST_DATABASE_HOST || 'devops-global-postgres',
-  dialect: 'postgres',
-};
